f73f7294578b42ef75dc9f912bd664842259dde7
Evin M Padhi <evinmpadhi@mac.lan>
2026-09-05 16:24:42 -0700

    Clarify sample averaging for digest commit trial

--- scripts/cell_type_specific_expression/filter_cell_type_beds.R
+++ scripts/cell_type_specific_expression/filter_cell_type_beds.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 
-# BED inputs use linear CPM. Expression thresholds use mean log2(CPM + 1).
+# BED inputs use linear CPM.
+# Expression thresholds use the mean of log2(CPM + 1) across samples.
 file_arg <- grep("^--file=", commandArgs(trailingOnly = FALSE), value = TRUE)
 script_path <- gsub("~+~", " ", sub("^--file=", "", file_arg[[1L]]), fixed = TRUE)
 source(file.path(dirname(normalizePath(script_path)), "bootstrap.R"))
